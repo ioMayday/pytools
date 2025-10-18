cdf5f8753d46c519accb89c37c06725f73d4d3a4
ioMayday <zhenbukexue@hotmail.com>
2025-10-18 10:36:07 +0800

    1、增加自由现金流折现计算

--- samples_utils.py
+++ samples_utils.py
@@ -1,10 +1,24 @@
 import utils.pytool_utils as pyutils
 
-# test demo
-yr = pyutils.profit_to_year_rate(7.73, 1000, 90)
-output_str = 'year rate: {:.3}%'.format(yr * 100)
-print(output_str)
 
-mon = pyutils.year_rate_to_profit(0.03135, 1000, 90) # 3.1%
-output_str = 'days profit: {:.3}'.format(mon)
-print(output_str)
+if __name__ == "__main__":
+    # test demo
+    yr = pyutils.profit_to_year_rate(7.73, 1000, 90)
+    output_str = 'year rate: {:.3}%'.format(yr * 100)
+    print(output_str)
+
+    mon = pyutils.year_rate_to_profit(0.03135, 1000, 90) # 3.1%
+    output_str = 'days profit: {:.3}'.format(mon)
+    print(output_str)
+
+    res = profit_to_year_rate(0.24, 6155, 1)
+    print("\n")
+    print(res)
+
+    res = year_rate_to_profit(0.06, 5000, 21)
+    print("\n")
+    print(res)
+
+    res = calc_dcf_total(10, 10, 0.06) # output: 78.01
+    print("\n")
+    print(res)
--- utils/pytool_utils.py
+++ utils/pytool_utils.py
@@ -15,7 +15,7 @@ def profit_to_year_rate(profit, invest, day):
 
 
 def year_rate_to_profit(year_rate, invest, day):
-    """日收益到年利率计算
+    """年利率到日收益计算
     year_rate: 年利率
     invest: 本金
     day:    投入天数
@@ -24,3 +24,19 @@ def year_rate_to_profit(year_rate, invest, day):
     year_profit = year_rate * invest
     profit = year_profit / CONST_YEAR_DAYS * day
     return profit
+
+
+def calc_dcf_total(profit, years, discount_rate):
+    """dcf现金流计算
+    profit: 年利润
+    years:    投入年数
+    discount_rate: 折现率
+    return: 折现到现价
+    """
+    # 假设固定profit
+    dcf = 0
+    for i in range(years):
+        dcf = dcf + profit / ((1 + discount_rate)**i)
+    return dcf
+
+
